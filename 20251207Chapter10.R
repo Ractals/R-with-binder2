296f50617de607ddb3a7b86e956705f00bac95e4
shuhei <xxx@gmail.com>
2025-12-07 10:21:36 +0000

    commit 19:21

--- 20251207Chapter10.R
+++ 20251207Chapter10.R
@@ -50,10 +50,15 @@ str_glue ("Data include {nrow (linelist)} cases and are current to {format (Sys.
 #we can include multiple pairs of curly braces 
 #in a single str_glue () command. 
 #####################################
-
+names (linelist)
 
 str_glue ("Linelist as of {current_date}.\nLast case hospitalized on {last_hospital}.\n{n_missing_onset} cases are missing date of onset and not shown",
-          current_date = firnat (Sys.Date (), '%d %b %Y'),
-          last_hospital = format (as.Date (max (linelist$date_hospitalisation, na.rm = T)), '%d %b%Y'),
+          current_date = format (Sys.Date (), '%d %b %Y'),
+          last_hospital = format (as.Date (max (linelist$date_hospitalisation, na.rm = T)), '%d %b %Y'),
           n_missing_onset = nrow (linelist |> filter (is.na (date_onset)))
           )
+#I can't run it - it shows "Error: object 'date_onset' not fount."
+#I don't get it. date_onset definitely exists as a variable, 
+#but is it supposed to be an object?
+
+
