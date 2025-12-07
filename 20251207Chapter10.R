32bd7d3fe96997a7830a80c39fde83cb4c119ecb
shuhei <xxx@gmail.com>
2025-12-07 15:52:42 +0000

    25:52

--- 20251207Chapter10.R
+++ 20251207Chapter10.R
@@ -47,7 +47,7 @@ str_glue ("Data include {nrow (linelist)} cases and are current to {format (Sys.
 #####################################
 # References to dynamic code or predefined values are placed
 #inside curly braces ｛｝within double quotes. 
-#we can include multiple pairs of curly braces 
+#you can include multiple pairs of curly braces 
 #in a single str_glue () command. 
 #####################################
 names (linelist)
@@ -56,9 +56,103 @@ str_glue ("Linelist as of {current_date}.\nLast case hospitalized on {last_hospi
           current_date = format (Sys.Date (), '%d %b %Y'),
           last_hospital = format (as.Date (max (linelist$date_hospitalisation, na.rm = T)), '%d %b %Y'),
           n_missing_onset = nrow (linelist |> filter (is.na (date_onset)))
-          )
+)
 #I can't run it - it shows "Error: object 'date_onset' not fount."
 #I don't get it. date_onset definitely exists as a variable, 
 #but is it supposed to be an object?
 
 
+#Extracting from a data frame
+install.packages ("tidyverse")
+library ("tidyverse")
+case_table <- tibble (
+  zone = c("Zone 1", "Zone 2", "Zone 3", "Zone 4", "Zone 5"),
+  new_cases = c(3, 0, 7, 0, 15),
+  total_cases = c(40, 4, 25, 10, 103)
+)
+case_table
+
+case_table |>
+  str_glue_data ("{zone}: {new_cases} ({total_cases} total cases)")
+
+
+#Concatenate strings from different rows
+str_c (case_table$zone, case_table$new_cases, sep = " = ", collapse = "; ")
+
+#By wrapping the previously written str_c () with another str_c (), 
+#you can add the string "New Cases:" to the beginning of the sentence. 
+#(If "New Cases;" were inside the original str_c (), 
+#it would be displayed multiple times.)
+
+str_c ("New Cases: ", str_c (case_table$zone, case_table$new_cases, sep = " = ", collapse = "; "))
+
+
+
+#mergeing columns
+df <- tibble (
+  case_ID = c(1:6),
+  symptoms = c("jaundice, fever, chills",  #patient No.1
+               "chills, aches, pains",     #patinet No.2
+               "fever",                      #patient No.3
+               "vomiting, diarrhooea",      #patient No.4
+               "bleeding from gums, fever", #patient No.5
+               "rapid pulse, headache"     #patient No.6
+  ),
+  outcom = c("Recover", "Death", "Death", "Recover", "Recover", "Recover")
+)
+
+df
+#??separate
+install.packages("tidyr")
+library(tidyr)
+separate
+
+df_split <- separate (df, symptoms, into = c("sym_1", "sym_2", "sym_3"), extra = "merge")
+df_split
+
+
+#below as to avoid the warning
+df_split <- separate (
+  df, symptoms, into = c("sym_1", "sym_2", "sym_3"), 
+  extra = "merge", 
+  fill = "right" # to avoid the warning: fill the missing values with NA on the right
+)
+df_split
+
+
+#split
+
+str_split (string = "jaundice, fever, chills", pattern = ",")
+
+
+pt1_symptoms <- str_split ("jaundice, fever, chills", ",")
+pt1_symptoms [[1]][2] #Return the second value inside the first (and in this case, only) element of the list
+#In the example above, since it is a list, the first (and in this case, the only) element is essentially the first object.
+
+
+
+#When multiple strings are supplied to str_split (), the resulting list contains multiple elements.
+symptoms <- c("jaundice, fever, chills", 
+              "chills, aches, pains",
+              "fever",
+              "vomiting diarrhoea",
+              "bleeding from gums, fever",
+              "rapid, pulse, headache")
+str_split (symptoms, ",") #Split the symptoms for each patient
+
+
+
+#If you want to later convert the output into data frame columns,
+#it is useful to set simplify = TRUE, which outputs a character matrix.
+str_split (symptoms, ",", simplify = TRUE)
+
+#You can also use n = to control the number of splits. For example, the following example limits the number of splits to 2,
+#and any additional commas remain inside the second value.
+str_split (symptoms, ",", simplify = TRUE, n = 2)
+
+
+#なんかcmmiteもできないし、
+#実行もできなくなったからここまで#だな。
+#毎回commiteしないとプログラムの安全を確保できないう#えに、
+#こうも不安定だと使い物にならないね。
+#～20:49
