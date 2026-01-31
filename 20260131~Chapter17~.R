6722550e391a1ba48d187e1a9f8fff19abbf9dab
shuhei <xxx@gmail.com>
2026-01-31 05:39:56 +0000

    20260131 14:39

--- 20260131~Chapter17~.R
+++ 20260131~Chapter17~.R
@@ -8,6 +8,9 @@
 #install.packages
 
 install.packages ("pacman")
+library (pacman)
+
+#p_load
 pacman::p_load (
   rio,
   here,
@@ -27,12 +30,47 @@ linelist <- readRDS ("linelist_cleaned.rds")
 #install.packages ("openxlsx")
 #library (openxlsx)
 #linelist <- read.xlsx ("linelist_cleaned.xlsx")
-
-
 head (linelist, 5)
+
+
 #17.2 Viewing the Data
+#Retrieve informating for each variable in the dataset
+skim (linelist)
 
 #Using the skimer package and rstatix / gtsummary's
 #get_summary_stats (), you can obtain summary statistics in data frame format.
 #While it is also possible to use base R's sumary () function to get an overview of the entire dataset, 
 #it's output cam be harder to read compared with the results produced by skimr.
+
+## Retrive information for each column in the dataset
+summary (linelist)
+
+
+#summary statistics
+summary (linelist$age_years)
+
+
+summary (linelist$age_years)[[2]]
+
+
+#Using restatix / gtsummary's get_summary_stats (), uyou can obtain summary statistics in data frame format.
+linelist2 <- linelist |>
+  get_summary_stats (
+    age, wt_kg, ht_cm, ct_blood, temp, # Row where the calculations are performed
+    type = "common" #Specify the summary statistics to be used
+  )
+linelist2
+
+
+#17.3 The Janitor Package
+#Basic Usage of tabyl ()
+linelist3 <- linelist |>
+  tabyl (age_cat)
+linelist3
+
+
+
+#Cross-tabulation table
+linelist3 <- linelist |>
+  tabyl (age_cat, gender)
+linelist3
