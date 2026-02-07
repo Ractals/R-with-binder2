2ff7102d57701308ac31b861aba29d50f2fca22c
shuhei <xxx@gmail.com>
2026-02-07 04:40:29 +0000

    20260207

--- 20260207~ourR_Chapter8~.R
+++ 20260207~ourR_Chapter8~.R
@@ -8,6 +8,17 @@ library (pacman)
 #8.1 input data
 #8.1.1 In the case of an CSV file
 download.file ("https://www.jaysong.net/RBook/Data/FIFA_Women.csv", "FIFA_Women.csv")
-
 my_df1 <- read.csv ("FIFA_Women.csv")
-head (my_df1)
+head (my_df1, 10)
+tail (my_df1, n = 9) # last n (= 9) rows
+
+#8.1.2 About encoding
+download.file ("https://www.jaysong.net/RBook/Data/Vote_ShiftJIS.csv", "Vote_ShiftJIS.csv")
+ShifftJIS_df <- read.csv ("Vote_ShiftJIS.csv")
+#> ShifftJIS_df <- read.csv ("Vote_ShiftJIS.csv")
+#Error in type.convert.default(data[[i]], as.is = as.is[i], dec = dec,  : 
+#                                invalid multibyte string at '<96>k<8a>C<93><b9>'
+ShifftJIS_df <- readr::read_csv ("Vote_ShiftJIS.csv")
+tail (ShifftJIS_df, n = 5)
+
+
