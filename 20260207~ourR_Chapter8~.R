1a7f284a3bd800c185b6dcdf03a5430f3c90e791
shuhei <xxx@gmail.com>
2026-02-07 04:52:39 +0000

    20260207 13:52

--- 20260207~ourR_Chapter8~.R
+++ 20260207~ourR_Chapter8~.R
@@ -14,11 +14,24 @@ tail (my_df1, n = 9) # last n (= 9) rows
 
 #8.1.2 About encoding
 download.file ("https://www.jaysong.net/RBook/Data/Vote_ShiftJIS.csv", "Vote_ShiftJIS.csv")
-ShifftJIS_df <- read.csv ("Vote_ShiftJIS.csv")
+ShiftJIS_df <- read.csv ("Vote_ShiftJIS.csv")
 #> ShifftJIS_df <- read.csv ("Vote_ShiftJIS.csv")
 #Error in type.convert.default(data[[i]], as.is = as.is[i], dec = dec,  : 
 #                                invalid multibyte string at '<96>k<8a>C<93><b9>'
-ShifftJIS_df <- readr::read_csv ("Vote_ShiftJIS.csv")
-tail (ShifftJIS_df, n = 5)
+ShiftJIS_df <- readr::read_csv ("Vote_ShiftJIS.csv")
+tail (ShiftJIS_df, n = 5)
+
+
+#1.Specifying the fileEncoding argument in the read.csv() function
+ShiftJID_df2 <- read.csv ("Vote_ShiftJIS.csv", fileEncoding = "Shift_JIS")
+head (ShiftJID_df2)
+
+#When readin g a UTF-8 file on Windows, it is recommended to specify fileEncoding = "UTF-8"
+
+#2.Specifying the locale argument in the read_csv() function
+#??locale
+ShiftJIS_df3 <- readr::read_csv ("Vote_ShiftJIS.csv", locale = readr::locale (encoding = "Shift_JIS"))
+head (ShiftJIS_df3)
+
 
 
