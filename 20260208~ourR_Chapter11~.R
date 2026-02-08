c7a4161dd1d4b32912b741b49a537235369af753
shuhei <xxx@gmail.com>
2026-02-08 07:36:31 +0000

    20260208

---
+++ 20260208~ourR_Chapter11~.R
@@ -0,0 +1,60 @@
+#20260208 16:22~
+#11 Basics of R programing
+#11.1 Basic concepts of the R language
+#11.1.1 Objects
+my_vec1 <- c(1, 2, 3, 4, 5)
+my_vec1*2
+
+#11.1.2 Class
+class  (my_vec1)
+class ("*")
+class ('*')
+#class (*)
+class (merge)
+class (2)
+class (class)
+
+
+#11.1.3 Functions and arguments
+my_vec2 <- c(1, 2, 3, NA, 5)
+
+sum (my_vec2, na.rm = T)
+
+
+#11.2 R coding style
+
+#11.2.1 Object name
+
+
+#Avoid reserved words
+#Emphasize brevity and clarity
+
+#11.2.2 Line breaks
+#11.2.3 Spaces and indentation
+
+#An evil example
+#Good example
+#Bad example
+
+#11.2.4 Assignment
+
+
+
+
+#11.3 Iteration
+#11.3.1 Iteration using for
+#Nested for loops
+#11.3.2 Iteration using while
+#~
+
+
+
+
+#11.4 Conditional branching
+
+
+#11.5 Exercises
+
+
+
+
