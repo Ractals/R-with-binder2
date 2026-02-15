7dbf9f5e7633c4b5553f265f241ab6409854bb62
shuhei <xxx@gmail.com>
2026-02-15 06:07:21 +0000

    20260215 15:07

--- 20260215~ourR_Chapter12~.R
+++ 20260215~ourR_Chapter12~.R
@@ -65,9 +65,60 @@ mySum2 (na.rm = TRUE, c(1, 2, 3, NA, 5))
 
 
 
-#12.2 A little complex function
+#12.2 A slightly more complex function
+#Define the DQ_Attack function
+  ## List of arguments
+  ## attack: Hero's strength + weapon attack power (numeric vector of length 1)
+  ## defence: Enemy's defense (numeric vector of length 1)
+  ## hp:Enemy's HP (numeric vector of length 1)
+  ## enemy: Enemy's name (charactor vector of length 1)
 
+  # Base damage value
 
-#12.3
+  # If the base damage is negative , set it to 0
 
+  # Damage range
 
+  # Minimum damage
+
+  # If the minimum damage is negative, set it to 0
+
+  # Maximum damage
+
+  # Store the enemy's remaining HP
+
+  # While the remaining HP is greater then 0, repeat the following:
+
+    # Randomly drow one value between the minimum and maximum damage
+
+    # Round to one decimal place
+
+    # Updata the remaining HP
+
+    # Display the message
+
+    print (paste0 (enemy " takes ", Damege, " damage!!"))
+    
+    # After the loop ends, display the victor message
+    paste0 ("Defeated ", enemy, "!")
+
+    
+    #Slime Knight
+#12.3 Function inside a function
+
+  # Create an empty vector of length n+1 (including the seed)
+  # Store the seed in the first element
+# Perform a loop, assigning values from 2 to n+1 to i sequentially
+    # Store in the i-th element of rng_vec a pseudo-random number
+    # generated using the (i-1) -th element as the seed
+    
+    
+    # Remove the first element (the seed)
+    # Divide by m so that values range between 0 and a
+    
+    # Return thenresult
+
+    
+    
+hist (LCG_Numbers, xlab = "Randm Numbers", ylab = "Frequencey",
+      main = "Histogram of 10,000 Generated Random Numbers")
