5aa97fcc1c707837b8e52ab26240a0a83071a26b
shuhei <xxx@gmail.com>
2026-02-15 07:38:49 +0000

    20260215

---
+++ 20260215~ourR_Chapter13~.R
@@ -0,0 +1,44 @@
+#20260215 16:00~
+
+#13 Data Handling [Extract]
+#13.1 Data Handling and tidyverse
+#13.2 The Pipe Operater (|>)
+#13.3 Selecting columns
+
+#13.3.1 Selecting Specific columns
+
+
+#13.3.2 Selecting Specific columns and Renaming Them
+
+#13.3.3 Excluding Specific columns
+
+
+#13.3.4 Selecting Adjacent columns
+
+#13.3.5 Reordering Only Certain columns
+
+
+#~
+
+#13.3.6 Ysefyk Features of select ()
+
+  # starts_with (), ends_with(), contains(), num_range()
+  # all_of () and any_of ()
+  # last_col ()
+  # where ()
+
+#13.4 Selecting Rows
+#13.4.1 Selecting Specific Rows
+
+  # slice ()
+  # sline_head ()
+  # sline_tail ()
+  # sline_max ()
+  # sline_min ()
+  # sline_sample ()
+
+#13.4.2 Selecting Rows That Meet Certain Conditions
+
+
+
+#13.5 Sorting rows
