3ac0d2cb0d2208657ec8def44482e94ba88bd1b3
shuhei <xxx@gmail.com>
2026-06-08 16:50:47 +0000

    20260608

--- Learning_For_Loops_with_AI.R
+++ Learning_For_Loops_with_AI.R
@@ -116,3 +116,136 @@ for (j in names (df2)) {
 df2
 
 #20260607 ~24:18
+
+
+
+
+
+
+#20260608 25:25~
+x <- 10
+if (x > 5) {
+  print (">5")
+}
+
+x <- 3
+if (x >5) {
+  print (">5")
+}
+
+x <- 10
+if (x == 10) {
+  print ("it's 10")
+}
+
+x <- 3
+if (x > 5) {
+  print (">5")
+}else {
+  print ("<5")
+}
+
+score <- 75
+if (score >= 90) {
+  print ("A")
+} else if (score >= 70) {
+  print ("B")
+}else {
+  print ("C")
+}
+
+score <- 95
+if (score >= 70) {
+  print ("B")
+}else if (score >= 90) {
+  print ("A")
+}
+
+age <- 25
+if (age >= 20 & age < 30) {
+  print ("20 <= age < 30")
+}
+
+
+x <- 100
+if (x < 0 | x > 50) {
+  print ("out of range")
+}
+
+
+x <- 5
+if (! (x > 10)) {
+  print ("<10")
+}
+
+
+age <- 43
+if (age < 20) {
+  grp <- "child"
+}else if (age < 40) {
+  grp <- "Adult"
+}else{
+  grp <- "Senior"
+}
+grp
+
+
+for (i in 1:10) {
+  if (i %% 2 ==0)
+    print (i)
+}
+
+
+x <- NA
+if (is.na (x)) {
+  print ("missing")
+}
+
+
+score <- 82
+if (score > 90) {
+  print ("A")
+}else if (score > 80) {
+  print ("B")
+}else if (score > 70) {
+  print ("C")
+}else {
+  print ("D")
+}
+
+
+
+age <- c (15, 22, 35, 47, 68)
+range <- NULL
+if (age < 20) {
+  range <- paste0 (range, "Minor")
+}else if (age < 65) {
+  range <- paste0 (range,"Adult")
+}else {
+  range <- paste0 (range, "Senior")
+}
+range
+
+
+age <- c (15, 22, 35, 47, 68)
+if (age < 20) {
+  range <-"Minor"
+}else if (age < 65) {
+  range <- "Adult"
+}else {
+  range <- "Senior"
+}
+range
+
+for (i in 1:length (age)) {
+  if (age[i] < 20) {
+    print ("Minor")
+  }else if (age[i] < 65) {
+    print ("Adult")
+  }else {
+    print ("Senior")
+  }
+}
+
+
+#20260608 ~25:49
